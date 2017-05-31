e205402a074e02ede64dc3ed59f1ffb7b660086e
MikelSage <michael.sagapolutele@yahoo.com>
2017-05-31 14:03:31 -0600

    add initialize method

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -1,3 +1,7 @@
 class Chisel
-  
+  attr_reader :input_file, :output_file
+  def initialize(paths)
+    @input_file = paths[0]
+    @output_file = paths[1]
+  end
 end
--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -5,10 +5,14 @@ require_relative '../lib/chisel.rb'
 class ChiselTest < Minitest::Test
   attr_reader :chisel
   def setup
-    @chisel = Chisel.new
+    @chisel = Chisel.new(['./data/my_input.md', './data/my_output.html'])
   end
 
-  def test_it_exists
+  def test_it_exists_and_knows_its_file_paths
     assert_instance_of Chisel, chisel
+    assert_equal './data/my_input.md', chisel.input_file
+    assert_equal './data/my_output.html', chisel.output_file
   end
+
+  
 end
